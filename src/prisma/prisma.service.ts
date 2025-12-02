bed60fe8560bd139bf68e4d5db8a291a93b7acad
alex <alex@gmail.com>
2025-12-02 18:51:45 -0300

    fix: prisma service e prisma client atualizados

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,16 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma'; // IMPORTA DO CLIENT GERADO
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
-    // Prisma 7 NAO USA MAIS $connect()
-    await this.$transaction([]); // força inicialização segura
+    await this.$connect();
   }
 
   async onModuleDestroy() {
-    // Prisma 7 NAO USA MAIS $disconnect()
     await this.$disconnect();
   }
 }
